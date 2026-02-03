d76b1e2df664c129afbf0598d5866435c7eb5167
t-fan-gif <inotijwork6@gmail.com>
2026-02-03 14:29:42 +0900

    revert: remove URL strategy changes, will address in native app version

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'routing/app_router.dart';
 
 void main() {
-  // Webの場合、ハッシュモードを使用してブラウザ履歴問題を解消
-  usePathUrlStrategy();
   runApp(const ProviderScope(child: LongDistanceDiaryApp()));
 }
 
