83c8f730321a787e461cd5df45cb851f08a1afa7
t-fan-gif <inotijwork6@gmail.com>
2026-02-03 13:58:52 +0900

    test: add usePathUrlStrategy to fix browser back gesture issue

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'routing/app_router.dart';
 
 void main() {
+  // Webの場合、ハッシュモードを使用してブラウザ履歴問題を解消
+  usePathUrlStrategy();
   runApp(const ProviderScope(child: LongDistanceDiaryApp()));
 }
 
