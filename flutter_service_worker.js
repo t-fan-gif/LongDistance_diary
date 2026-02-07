15063ef766b1cb001c82bce2b4b40752b8f8b8b9
t-fan-gif <inotijwork6@gmail.com>
2026-02-07 15:54:33 +0900

    Deploy v1.3.20+2 with load calculation improvements and QR fixes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,20 +26,20 @@ const RESOURCES = {"assets/AssetManifest.bin": "8abccb6ba8086e07b7eaa5da9437a3c5
 "drift_worker_v2301.js": "5b025027f86dce2d863284853eeed485",
 "favicon.png": "7a8228743b3faace188024c6d0dea645",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "d53774b1739c9fb0ef3942864c28f442",
+"flutter_bootstrap.js": "120255f678b8f1b97a90ab2648aec0bb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/icon.png": "8d8045c90578bc8922e0623f5972bdf4",
 "icons/icon2.png": "7a8228743b3faace188024c6d0dea645",
-"index.html": "6496e267cfa4658d420705687ca4a76e",
-"/": "6496e267cfa4658d420705687ca4a76e",
-"main.dart.js": "1a1b4d1871021b61745e3b3694e1b62e",
+"index.html": "d659b6d4ae8074e7488b480cd01fdfc1",
+"/": "d659b6d4ae8074e7488b480cd01fdfc1",
+"main.dart.js": "6657bcb0ee377e125b3d09e4e402dc7c",
 "manifest.json": "593bc49e57ab3670f593b7677d7f20a5",
 "sqlite3.wasm": "4e83521cba4c5d835fc6fbab60feb6e4",
 "sqlite3_v294.wasm": "2e9fc1ccbb9d15199fccf405b0ceee53",
-"version.json": "a9d9a369898b4ded199daa9bb7042b39"};
+"version.json": "c9e681d674b535fd58a1729a37994385"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
