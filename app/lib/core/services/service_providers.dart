911d4bc75b13659a306a831fc77f60b05f8cb8af
t-fan-gif <inotijwork6@gmail.com>
2026-02-16 14:06:24 +0900

    Define sessionRepositoryProvider

--- app/lib/core/services/service_providers.dart
+++ app/lib/core/services/service_providers.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../db/db_providers.dart';
 import '../repos/personal_best_repository.dart';
+import '../repos/session_repository.dart';
 import 'vdot_calculator.dart';
 import 'training_pace_service.dart';
 import 'load_calculator.dart';
@@ -17,6 +18,11 @@ final loadCalculatorProvider = Provider<LoadCalculator>((ref) {
 final personalBestRepositoryProvider = Provider<PersonalBestRepository>((ref) {
   final db = ref.watch(appDatabaseProvider);
   return PersonalBestRepository(db);
+});
+
+final sessionRepositoryProvider = Provider<SessionRepository>((ref) {
+  final db = ref.watch(appDatabaseProvider);
+  return SessionRepository(db);
 });
 
 final trainingPaceServiceProvider = Provider<TrainingPaceService>((ref) {
